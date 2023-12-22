2b522ccd3a32418c9c425435c9eb71bdcf40f96b
Jorge Cortes <77340382+chochelius@users.noreply.github.com>
2023-12-22 19:03:26 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='App'>
+        <h1>React App</h1>
+
+        <p className='text-light bg-purple-300'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+          voluptates, voluptatum, quidem, quas, cumque voluptate corporis
+          voluptatibus quos accusantium quae magni. Quisquam, voluptatem
+          voluptas. Quisquam, voluptatem voluptas.
+
+        </p>
+
+        <p className="text-light bg-purple-300">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+          voluptates, voluptatum, quidem, quas, cumque voluptate corporis
+          voluptatibus quos accusantium quae magni. Quisquam, voluptatem
+          voluptas. Quisquam, voluptatem voluptas.
+        </p>
+        
+
+      </div>
+    </>
+  )
+}
+
+export default App
